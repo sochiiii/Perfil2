0ffdb60aec78c86ac4bfa962249e3bec9d4bbab9
sochiiii <108245333+sochiiii@users.noreply.github.com>
2024-02-28 11:05:37 -0600

    Update eje.sql

--- eje.sql
+++ eje.sql
@@ -3,44 +3,44 @@ DROP DATABASE IF EXISTS libro_Express;
 CREATE DATABASE libro_express;
 USE libro_express;
 
-CREATE TABLE clientes(
+CREATE TABLE tb_clientes(
 	id_cliente VARCHAR(36) PRIMARY KEY DEFAULT (UUID()) ,
     nombre_cliente VARCHAR(50) NOT NULL,
     email_cliente VARCHAR(100) NOT NULL,
-    CONSTRAINT uc_email_cliente UNIQUE(email_cliente),
+    CONSTRAINT unique_email_cliente UNIQUE(email_cliente),
     telefono VARCHAR(10) NOT NULL,
-    CONSTRAINT uc_telefono UNIQUE(telefono)
+    CONSTRAINT unique_telefono UNIQUE(telefono)
 );
 
-CREATE TABLE genero_libros(
+CREATE TABLE tb_genero_libros(
 	id_genero_libro VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
     nombre_genero_libro VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE prestamos(
+CREATE TABLE tb_prestamos(
 	id_prestamo VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
     id_cliente VARCHAR(36) NOT NULL,
-    CONSTRAINT id_cliente_prestamo FOREIGN KEY(id_cliente) REFERENCES clientes(id_cliente),
+    CONSTRAINT id_cliente_prestamo FOREIGN KEY(id_cliente) REFERENCES tb_clientes(id_cliente),
     fecha_inicio DATE NOT NULL,
     fecha_devolucion DATE NOT NULL,
-    CONSTRAINT chk_fecha CHECK (fecha_inicio < fecha_devolucion),
+    CONSTRAINT check_fecha CHECK (fecha_inicio < fecha_devolucion),
     estado ENUM('activo', 'inactivo')
 );
 
-CREATE TABLE libros(
+CREATE TABLE tb_libros(
 	id_libro VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
     titulo_libro VARCHAR(50) NOT NULL,
     anio_publicacion INT NOT NULL,
-    CONSTRAINT chk_anio_publicacion CHECK(anio_publicacion > 0 AND anio_publicacion < 2025),
+    CONSTRAINT check_anio_publicacion CHECK(anio_publicacion > 0 AND anio_publicacion < 2025),
     id_genero_libro VARCHAR(36) NOT NULL,
-    CONSTRAINT id_genero_tblibro FOREIGN KEY (id_genero_libro) REFERENCES genero_libros(id_genero_libro),
+    CONSTRAINT id_genero_tblibro FOREIGN KEY (id_genero_libro) REFERENCES tb_genero_libros(id_genero_libro),
     estado ENUM('disponible', 'prestado')
 );
 
-CREATE TABLE detalles_prestamos(
+CREATE TABLE tb_detalles_prestamos(
 	id_detalle_prestamo VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
     id_prestamo VARCHAR(36) NOT NULL,
-	CONSTRAINT id_prestamo_detalle FOREIGN KEY (id_prestamo) REFERENCES prestamos(id_prestamo),
+	CONSTRAINT id_prestamo_detalle FOREIGN KEY (id_prestamo) REFERENCES tb_prestamos(id_prestamo),
     id_libro VARCHAR(36) NOT NULL,
-    CONSTRAINT id_libro_detalle FOREIGN KEY (id_libro) REFERENCES libros(id_libro)
+    CONSTRAINT id_libro_detalle FOREIGN KEY (id_libro) REFERENCES tb_libros(id_libro)
 );
